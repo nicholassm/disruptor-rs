9eef100037b5aa1b3721532384c17548b7483158
nicolas carrara <nicolas.carrara@myrill.io>
2024-04-26 22:16:49 +0200

    making *Producer and *Barrier public

--- src/lib.rs
+++ src/lib.rs
@@ -94,238 +94,241 @@ mod builder;
 pub use crate::builder::{build_single_producer, build_multi_producer, ProcessorSettings};
 pub use crate::producer::{Producer, RingBufferFull};
 pub use crate::wait_strategies::{BusySpin, BusySpinWithSpinLoopHint};
+pub use crate::producer::multi::{MultiProducer};
+pub use crate::producer::single::{SingleProducer};
+pub use crate::consumer::{SingleConsumerBarrier, MultiConsumerBarrier};
 
 #[cfg(test)]
 mod tests {
-	use std::collections::HashSet;
-	use std::sync::mpsc;
-	use std::thread;
-	use super::*;
+    use std::collections::HashSet;
+    use std::sync::mpsc;
+    use std::thread;
+    use super::*;
 
-	#[derive(Debug)]
-	struct Event {
-		num: i64,
-	}
+    #[derive(Debug)]
+    struct Event {
+        num: i64,
+    }
 
-	#[test]
-	#[should_panic(expected = "Size must be power of 2.")]
-	fn size_not_a_factor_of_2() {
-		build_single_producer(3, || { 0 }, BusySpin);
-	}
+    #[test]
+    #[should_panic(expected = "Size must be power of 2.")]
+    fn size_not_a_factor_of_2() {
+        build_single_producer(3, || { 0 }, BusySpin);
+    }
 
-	#[test]
-	fn spsc_disruptor() {
-		let factory   = || { Event { num: -1 }};
-		let (s, r)    = mpsc::channel();
-		let processor = move |e: &Event, _, _| {
-			s.send(e.num).expect("Should be able to send.");
-		};
+    #[test]
+    fn spsc_disruptor() {
+        let factory = || { Event { num: -1 } };
+        let (s, r) = mpsc::channel();
+        let processor = move |e: &Event, _, _| {
+            s.send(e.num).expect("Should be able to send.");
+        };
 
-		let mut producer = build_single_producer(8, factory, BusySpin)
-			.handle_events_with(processor)
-			.build();
-		thread::scope(|s| {
-			s.spawn(move || {
-				for i in 0..10 {
-					producer.publish(|e| e.num = i*i );
-				}
-			});
-		});
+        let mut producer = build_single_producer(8, factory, BusySpin)
+            .handle_events_with(processor)
+            .build();
+        thread::scope(|s| {
+            s.spawn(move || {
+                for i in 0..10 {
+                    producer.publish(|e| e.num = i * i);
+                }
+            });
+        });
 
-		let result: Vec<_> = r.iter().collect();
-		assert_eq!(result, [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]);
-	}
+        let result: Vec<_> = r.iter().collect();
+        assert_eq!(result, [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]);
+    }
 
-	#[test]
-	fn pipeline_of_two_spsc_disruptors() {
-		let factory   = || { Event { num: -1 }};
-		let (s, r)    = mpsc::channel();
-		let processor = move |e: &Event, _, _| {
-			s.send(e.num).expect("Should be able to send.");
-		};
+    #[test]
+    fn pipeline_of_two_spsc_disruptors() {
+        let factory = || { Event { num: -1 } };
+        let (s, r) = mpsc::channel();
+        let processor = move |e: &Event, _, _| {
+            s.send(e.num).expect("Should be able to send.");
+        };
 
-		// Last Disruptor.
-		let mut producer = build_single_producer(8, factory, BusySpin)
-			.handle_events_with(processor)
-			.build();
-		let processor = move |e: &Event, _, _| {
-			producer.publish(|e2| e2.num = e.num );
-		};
+        // Last Disruptor.
+        let mut producer = build_single_producer(8, factory, BusySpin)
+            .handle_events_with(processor)
+            .build();
+        let processor = move |e: &Event, _, _| {
+            producer.publish(|e2| e2.num = e.num);
+        };
 
-		// First Disruptor.
-		let mut producer = build_single_producer(8, factory, BusySpin)
-			.handle_events_with(processor)
-			.build();
-		thread::scope(|s| {
-			s.spawn(move || {
-				for i in 0..10 {
-					producer.publish(|e| e.num = i*i );
-				}
-			});
-		});
+        // First Disruptor.
+        let mut producer = build_single_producer(8, factory, BusySpin)
+            .handle_events_with(processor)
+            .build();
+        thread::scope(|s| {
+            s.spawn(move || {
+                for i in 0..10 {
+                    producer.publish(|e| e.num = i * i);
+                }
+            });
+        });
 
-		let result: Vec<_> = r.iter().collect();
-		assert_eq!(result, [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]);
-	}
+        let result: Vec<_> = r.iter().collect();
+        assert_eq!(result, [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]);
+    }
 
-	#[test]
-	fn multi_publisher_disruptor() {
-		let factory   = || { Event { num: -1 }};
-		let (s, r)    = mpsc::channel();
-		let processor = move |e: &Event, _, _| {
-			s.send(e.num).expect("Should be able to send.");
-		};
+    #[test]
+    fn multi_publisher_disruptor() {
+        let factory = || { Event { num: -1 } };
+        let (s, r) = mpsc::channel();
+        let processor = move |e: &Event, _, _| {
+            s.send(e.num).expect("Should be able to send.");
+        };
 
-		let mut producer1 = build_multi_producer(8, factory, BusySpin)
-			.handle_events_with(processor)
-			.build();
-		let mut producer2 = producer1.clone();
+        let mut producer1 = build_multi_producer(8, factory, BusySpin)
+            .handle_events_with(processor)
+            .build();
+        let mut producer2 = producer1.clone();
 
-		let num_items = 100;
+        let num_items = 100;
 
-		thread::scope(|s| {
-			s.spawn(move || {
-				for i in 0..num_items/2 {
-					producer1.publish(|e| e.num = i);
-				}
-			});
+        thread::scope(|s| {
+            s.spawn(move || {
+                for i in 0..num_items / 2 {
+                    producer1.publish(|e| e.num = i);
+                }
+            });
 
-			s.spawn(move || {
-				for i in (num_items/2)..num_items {
-					producer2.publish(|e| e.num = i);
-				}
-			});
-		});
+            s.spawn(move || {
+                for i in (num_items / 2)..num_items {
+                    producer2.publish(|e| e.num = i);
+                }
+            });
+        });
 
-		let mut result: Vec<_> = r.iter().collect();
-		result.sort();
+        let mut result: Vec<_> = r.iter().collect();
+        result.sort();
 
-		let expected: Vec<i64> = (0..num_items).collect();
-		assert_eq!(result, expected);
-	}
+        let expected: Vec<i64> = (0..num_items).collect();
+        assert_eq!(result, expected);
+    }
 
-	#[test]
-	fn spmc_with_concurrent_consumers() {
-		let (s, r) = mpsc::channel();
+    #[test]
+    fn spmc_with_concurrent_consumers() {
+        let (s, r) = mpsc::channel();
 
-		let processor1 = {
-			let s = s.clone();
-			move |e: &Event, _, _| { s.send(e.num + 1).unwrap(); }
-		};
-		let processor2 = {
-			let s = s.clone();
-			move |e: &Event, _, _| { s.send(e.num + 2).unwrap(); }
-		};
-		let processor3 = {
-			move |e: &Event, _, _| { s.send(e.num + 3).unwrap(); }
-		};
+        let processor1 = {
+            let s = s.clone();
+            move |e: &Event, _, _| { s.send(e.num + 1).unwrap(); }
+        };
+        let processor2 = {
+            let s = s.clone();
+            move |e: &Event, _, _| { s.send(e.num + 2).unwrap(); }
+        };
+        let processor3 = {
+            move |e: &Event, _, _| { s.send(e.num + 3).unwrap(); }
+        };
 
-		let factory      = || { Event { num: -1 }};
-		let builder      = build_single_producer(8, factory, BusySpin);
-		let mut producer = builder
-			.pined_at_core(1).thread_named("my_thread").handle_events_with(processor1)
-			.handle_events_with(processor2)
-			.handle_events_with(processor3)
-			.build();
+        let factory = || { Event { num: -1 } };
+        let builder = build_single_producer(8, factory, BusySpin);
+        let mut producer = builder
+            .pined_at_core(1).thread_named("my_thread").handle_events_with(processor1)
+            .handle_events_with(processor2)
+            .handle_events_with(processor3)
+            .build();
 
-		producer.publish(|e| { e.num = 0; });
+        producer.publish(|e| { e.num = 0; });
 
-		drop(producer);
+        drop(producer);
 
-		let result: HashSet<i64> = r.iter().collect();
-		let expected = HashSet::from([1, 2, 3]);
-		assert_eq!(expected, result);
-	}
+        let result: HashSet<i64> = r.iter().collect();
+        let expected = HashSet::from([1, 2, 3]);
+        assert_eq!(expected, result);
+    }
 
-	#[test]
-	fn spmc_with_dependent_consumers() {
-		let (s, r) = mpsc::channel();
+    #[test]
+    fn spmc_with_dependent_consumers() {
+        let (s, r) = mpsc::channel();
 
-		let processor1 = {
-			let s = s.clone();
-			move |e: &Event, _, _| { s.send(e.num + 1).unwrap(); }
-		};
-		let processor2 = {
-			let s = s.clone();
-			move |e: &Event, _, _| { s.send(e.num + 2).unwrap(); }
-		};
-		let processor3 = {
-			move |e: &Event, _, _| { s.send(e.num + 3).unwrap(); }
-		};
+        let processor1 = {
+            let s = s.clone();
+            move |e: &Event, _, _| { s.send(e.num + 1).unwrap(); }
+        };
+        let processor2 = {
+            let s = s.clone();
+            move |e: &Event, _, _| { s.send(e.num + 2).unwrap(); }
+        };
+        let processor3 = {
+            move |e: &Event, _, _| { s.send(e.num + 3).unwrap(); }
+        };
 
-		let factory      = || { Event { num: -1 }};
-		let builder      = build_single_producer(8, factory, BusySpin);
-		let mut producer = builder
-			.handle_events_with(processor1)
-			.and_then()
-				.handle_events_with(processor2)
-				.and_then()
-					.handle_events_with(processor3)
-			.build();
+        let factory = || { Event { num: -1 } };
+        let builder = build_single_producer(8, factory, BusySpin);
+        let mut producer = builder
+            .handle_events_with(processor1)
+            .and_then()
+            .handle_events_with(processor2)
+            .and_then()
+            .handle_events_with(processor3)
+            .build();
 
-		producer.publish(|e| { e.num = 0; });
+        producer.publish(|e| { e.num = 0; });
 
-		drop(producer);
+        drop(producer);
 
-		let result: Vec<i64> = r.iter().collect();
-		assert_eq!(vec![1, 2, 3], result);
-	}
+        let result: Vec<i64> = r.iter().collect();
+        assert_eq!(vec![1, 2, 3], result);
+    }
 
-	#[test]
-	fn mpmc_with_dependent_consumers() {
-		let (s, r) = mpsc::channel();
+    #[test]
+    fn mpmc_with_dependent_consumers() {
+        let (s, r) = mpsc::channel();
 
-		let processor1 = {
-			let s = s.clone();
-			move |e: &Event, _, _| { s.send(e.num + 1).unwrap(); }
-		};
-		let processor2 = {
-			let s = s.clone();
-			move |e: &Event, _, _| { s.send(e.num + 2).unwrap(); }
-		};
-		let processor3 = {
-			let s = s.clone();
-			move |e: &Event, _, _| { s.send(e.num + 3).unwrap(); }
-		};
-		let processor4 = {
-			let s = s.clone();
-			move |e: &Event, _, _| { s.send(e.num + 4).unwrap(); }
-		};
-		let processor5 = {
-			let s = s.clone();
-			move |e: &Event, _, _| { s.send(e.num + 5).unwrap(); }
-		};
-		let processor6 = {
-			move |e: &Event, _, _| { s.send(e.num + 6).unwrap(); }
-		};
+        let processor1 = {
+            let s = s.clone();
+            move |e: &Event, _, _| { s.send(e.num + 1).unwrap(); }
+        };
+        let processor2 = {
+            let s = s.clone();
+            move |e: &Event, _, _| { s.send(e.num + 2).unwrap(); }
+        };
+        let processor3 = {
+            let s = s.clone();
+            move |e: &Event, _, _| { s.send(e.num + 3).unwrap(); }
+        };
+        let processor4 = {
+            let s = s.clone();
+            move |e: &Event, _, _| { s.send(e.num + 4).unwrap(); }
+        };
+        let processor5 = {
+            let s = s.clone();
+            move |e: &Event, _, _| { s.send(e.num + 5).unwrap(); }
+        };
+        let processor6 = {
+            move |e: &Event, _, _| { s.send(e.num + 6).unwrap(); }
+        };
 
-		let factory      = || { Event { num: -1 }};
-		let builder      = build_multi_producer(8, factory, BusySpin);
-		let mut producer1 = builder
-			.handle_events_with(processor1)
-			.handle_events_with(processor2)
-			.and_then()
-				.handle_events_with(processor3)
-				.handle_events_with(processor4)
-				.and_then()
-					.handle_events_with(processor5)
-					.handle_events_with(processor6)
-			.build();
+        let factory = || { Event { num: -1 } };
+        let builder = build_multi_producer(8, factory, BusySpin);
+        let mut producer1 = builder
+            .handle_events_with(processor1)
+            .handle_events_with(processor2)
+            .and_then()
+            .handle_events_with(processor3)
+            .handle_events_with(processor4)
+            .and_then()
+            .handle_events_with(processor5)
+            .handle_events_with(processor6)
+            .build();
 
-		let mut producer2 = producer1.clone();
+        let mut producer2 = producer1.clone();
 
-		thread::scope(|s| {
-			s.spawn(move || {
-				producer1.publish(|e| e.num = 0);
-			});
+        thread::scope(|s| {
+            s.spawn(move || {
+                producer1.publish(|e| e.num = 0);
+            });
 
-			s.spawn(move || {
-				producer2.publish(|e| e.num = 10);
-			});
-		});
+            s.spawn(move || {
+                producer2.publish(|e| e.num = 10);
+            });
+        });
 
-		let mut result: Vec<i64> = r.iter().collect();
-		result.sort();
-		assert_eq!(vec![1, 2, 3, 4, 5, 6, 11, 12, 13, 14, 15, 16], result);
-	}
+        let mut result: Vec<i64> = r.iter().collect();
+        result.sort();
+        assert_eq!(vec![1, 2, 3, 4, 5, 6, 11, 12, 13, 14, 15, 16], result);
+    }
 }

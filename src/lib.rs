aa2135750dbec12dce910e181fc996dd0fb80682
Nicholas Schultz-Møller <nicholassm@gmail.com>
2024-06-22 07:30:37 +0200

    Remove unnecessary main function from doctest.

--- src/lib.rs
+++ src/lib.rs
@@ -188,27 +188,25 @@
 //!     data: Rc<RefCell<i32>>
 //! }
 //!
-//! fn main() {
-//!     let factory = || { Event { price: 0.0 }};
-//!     let initial_state = || { State::default() };
+//! let factory = || { Event { price: 0.0 }};
+//! let initial_state = || { State::default() };
 //!
-//!     // Closure for processing events *with* state.
-//!     let processor = |s: &mut State, e: &Event, _: Sequence, _: bool| {
-//!         // Mutate your custom state:
-//!         *s.data.borrow_mut() += 1;
-//!     };
+//! // Closure for processing events *with* state.
+//! let processor = |s: &mut State, e: &Event, _: Sequence, _: bool| {
+//!     // Mutate your custom state:
+//!     *s.data.borrow_mut() += 1;
+//! };
 //!
-//!     let size = 64;
-//!     let mut producer = disruptor::build_single_producer(size, factory, BusySpin)
-//!         .handle_events_and_state_with(processor, initial_state)
-//!         .build();
+//! let size = 64;
+//! let mut producer = disruptor::build_single_producer(size, factory, BusySpin)
+//!     .handle_events_and_state_with(processor, initial_state)
+//!     .build();
 //!
-//!     // Publish into the Disruptor via the `Producer` handle.
-//!     for i in 0..10 {
-//!         producer.publish(|e| {
-//!             e.price = i as f64;
-//!         });
-//!     }
+//! // Publish into the Disruptor via the `Producer` handle.
+//! for i in 0..10 {
+//!     producer.publish(|e| {
+//!         e.price = i as f64;
+//!     });
 //! }
 //! ```
 

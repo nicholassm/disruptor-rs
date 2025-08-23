aaf138d49ee45a4108e9d7744f5bcfb5a5562c07
Nicholas Schultz-Møller <nicholassm@gmail.com>
2025-08-23 12:24:02 +0200

    Use method from std lib for power of 2 check.

--- src/ringbuffer.rs
+++ src/ringbuffer.rs
@@ -10,17 +10,13 @@ pub struct RingBuffer<E> {
 	index_mask: i64,
 }
 
-fn is_pow_of_2(num: usize) -> bool {
-	num != 0 && (num & (num - 1) == 0)
-}
-
 impl <E> RingBuffer<E> {
 	pub(crate) fn new<F>(size: usize, mut event_factory: F)
 	-> Self
 	where
 		F: FnMut() -> E
 	{
-		if !is_pow_of_2(size) { panic!("Size must be power of 2.") }
+		if !size.is_power_of_two() { panic!("Size must be power of 2.") }
 
 		let slots: Box<[UnsafeCell<E>]> = (0..size)
 			.map(|_i| UnsafeCell::new(event_factory()) )

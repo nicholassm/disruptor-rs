f59ca148f187d5a0d5227b522c390c2a8e7588a3
Nicholas Schultz-Møller <nicholassm@gmail.com>
2025-12-26 20:27:53 +0100

    Fix typo in doc.

--- src/wait_strategies.rs
+++ src/wait_strategies.rs
@@ -2,7 +2,7 @@
 //!
 //! The lowest latency possible is the [`BusySpin`] strategy.
 //!
-//! To "waist" less CPU time and power, use one of the other strategies which have higher latency.
+//! To "waste" less CPU time and power, use one of the other strategies which have higher latency.
 
 use std::hint;
 

193ca3bece518a4618d485bd990aa315bb5646e9
Mattia Papaccioli <mattiapapaccioli@gmail.com>
2026-05-16 18:48:02 +0200

    Fix import to export publicly

--- src/lib.rs
+++ src/lib.rs
@@ -306,7 +306,7 @@ mod producer;
 
 pub use crate::builder::{build_single_producer, build_multi_producer, ProcessorSettings};
 pub use crate::producer::{Producer, RingBufferFull, MissingFreeSlots};
-pub use crate::wait_strategies::{BusySpin, BusySpinWithSpinLoopHint};
+pub use crate::wait_strategies::{BusySpin, BusySpinWithSpinLoopHint, Sleep};
 pub use crate::producer::{single::{SingleProducer, SingleProducerBarrier}, multi::{MultiProducer,  MultiProducerBarrier}};
 pub use crate::consumer::{SingleConsumerBarrier, MultiConsumerBarrier};
 pub use crate::consumer::event_poller::{EventPoller, Polling, EventGuard, Branch};
